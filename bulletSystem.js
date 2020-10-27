7cced0ce8038f8d9f3b98c5cebc77b55af4fa30f
Danny Callaghan <dannycallaghan@gmail.com>
2020-10-27 20:23:16 +0000

    Proper vector rotation on ship and bullet direction

--- bulletSystem.js
+++ bulletSystem.js
@@ -2,7 +2,6 @@ class BulletSystem {
 
   constructor(){
     this.bullets = [];
-    this.velocity = new createVector(0, -5);
     this.diam = 10;
   }
 
@@ -12,22 +11,28 @@ class BulletSystem {
       this.edges();
   }
 
-  fire(x, y){
-    this.bullets.push(createVector(x,y));
+  fire(x, y, currentRotation){
+    const velocity = new createVector(0, -5);
+    velocity.rotate(radians(currentRotation));
+    this.bullets.push({
+      position: createVector(x,y),
+      velocity: velocity 
+    });
   }
 
   //draws all bullets
   draw(){
     fill(255);
     for (var i=0; i<this.bullets.length; i++){
-      ellipse(this.bullets[i].x, this.bullets[i].y, this.diam, this.diam);
+      ellipse(this.bullets[i].position.x, this.bullets[i].position.y, this.diam, this.diam);
     }
   }
 
   //updates the location of all bullets
   move(){
     for (var i=0; i<this.bullets.length; i++){
-      this.bullets[i].y += this.velocity.y;
+      this.bullets[i].position.x += this.bullets[i].velocity.x
+      this.bullets[i].position.y += this.bullets[i].velocity.y 
     }
   }
 
@@ -45,7 +50,10 @@ class BulletSystem {
   edges(){
     const bulletsLength = this.bullets.length;
     for (let i = 0; i < bulletsLength; i++) {
-      if ((this.bullets[i].x < 0 || this.bullets[i].x > width) || (this.bullets[i].y < 0 || this.bullets[i].y > height)) {
+      if (
+        (this.bullets[i].position.x < 0 || this.bullets[i].position.x > width) ||
+        (this.bullets[i].position.y < 0 || this.bullets[i].position.y > height)
+      ) {
         this.bullets.splice(i, 1);
         break;
       }
